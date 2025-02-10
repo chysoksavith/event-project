9b3ebf8c07890429bc27d0b49045cfdfc6f3f314
chysoksavith <120388241+chysoksavith@users.noreply.github.com>
2025-02-10 12:10:53 +0700

    complate model and migration of event project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
+use App\Models\City;
 use App\Models\User;
+use App\Models\Country;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +23,52 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $cambodia = Country::create([
+            'name' => 'Cambodia'
+        ]);
+        $china = Country::create([
+            'name' => 'china'
+        ]);
+
+        $cambodiaCities = [
+            'Phnom Penh',
+            'Siem Reap',
+            'Sihanoukville',
+            'Battambang',
+            'Kampot'
+        ];
+        $chinaCities = [
+            'Beijing',
+            'Shanghai',
+            'Guangzhou',
+            'Shenzhen',
+            'Chengdu'
+
+        ];
+        foreach ($cambodiaCities as $cityName) {
+            City::create([
+                'country_id' => $cambodia->id,
+                'name' => $cityName
+            ]);
+        }
+        foreach ($chinaCities as $cityName) {
+            City::create([
+                'country_id' => $china->id,
+                'name' => $cityName
+            ]);
+        }
+
+        $tags = [
+            'laravel',
+            'vue-js',
+            'livewire'
+        ];
+
+        foreach ($tags as $tag) {
+            Tag::create([
+                'name' => $tag,
+                'slug' => Str::slug($tag)
+            ]);
+        }
     }
 }
